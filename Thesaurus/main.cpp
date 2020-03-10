36dbe6160a90ef8c3f9de31578fbfae3d08466e0
ahmermughal <a2mughal143@gmail.com>
2020-03-10 09:40:14 +0500

    Initial Commit

---
+++ Thesaurus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Thesaurus
+//
+//  Created by Ahmer Mughal on 10/03/2020.
+//  Copyright © 2020 Ahmer Mughal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
